1ef83afa3821faf54fa8509a52d65d87fcee1bf8
wroberts <wroberts4@wisc.edu>
2019-12-04 10:30:35 -0600

    Remove numpy from yml and fix typo

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -96,7 +96,7 @@ setup_conda()
     conda env update -n jenkins_p2g_swbundle -f "${WORKSPACE}/build_environment.yml"
     # Documentation environment also has behave, while the build environment does not.
     conda remove -n jenkins_p2g_docs --all
-    conda env update -n jenkins_p2g_docs -f "${WORKSPACE}/build_environment.yml" -f
+    conda env update -n jenkins_p2g_docs -f "${WORKSPACE}/build_environment.yml"
     conda env update -n jenkins_p2g_docs -f "${WORKSPACE}/jenkins_environment.yml"
     conda activate jenkins_p2g_docs
     pip install -U --no-deps "$WORKSPACE"
